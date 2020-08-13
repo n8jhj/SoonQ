e0eb5f5824587a4e6e219116e1e9cafee009046c
n8jhj <nathaniel.j.jones@wsu.edu>
2020-08-13 16:14:57 -0700

    Add examples

---
+++ examples/timer_task.py
@@ -0,0 +1,47 @@
+"""Script for creating timer tasks.
+"""
+
+import time
+
+import soonq as sq
+
+
+def timer_sleep_all(interval, n):
+    for i in range(n):
+        timer_sleep(interval, i, n)
+
+
+def timer_sleep(interval, i=None, n=None):
+    if None not in (i, n):
+        msg = f"{i+1}/{n} "
+    else:
+        msg = ''
+    msg += f"Sleeping {interval} seconds..."
+    sq.echo(msg)
+    time.sleep(interval)
+
+
+class TimerTask(sq.BaseTask):
+    """Task to count to a certain number a specified number of times.
+    """
+
+    task_name = 'TimerTask'
+
+    def run(self, interval, n):
+        """Count at the given interval the given number of times.
+
+        Positional arguments:
+        interval - (int) Interval, in seconds.
+        n - (int) Number of times to count the given interval.
+        """
+        self.interval = interval
+        timer_sleep_all(interval, n)
+        sq.echo(f'Slept {interval * n} seconds total.')
+
+
+if __name__ == '__main__':
+    # Create a new task.
+    timer_task = TimerTask()
+    # Add the task to the queue, to be executed at some future time.
+    # Arguments to delay() are the same as for run().
+    timer_task.delay(3, 3)
---
+++ examples/timer_worker.py
@@ -0,0 +1,15 @@
+"""Script for runnning a Worker dedicated to accomplishing TimerTasks.
+"""
+
+import soonq as sq
+
+from timer_task import TimerTask
+
+
+if __name__ == "__main__":
+    # Instantiate TimerTask.
+    timer_task = TimerTask()
+
+    # Run worker.
+    worker = sq.Worker(task=timer_task)
+    worker.start()
