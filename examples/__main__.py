577c4da295ea2602b910e7727b47bd34d51a39a0
n8jhj <nathaniel.j.jones@wsu.edu>
2020-10-05 17:40:03 -0700

    Make self-made help option for examples

--- examples/__main__.py
+++ examples/__main__.py
@@ -1,13 +1,29 @@
+"""Logic for when this module is called from the command line.
+"""
+
+import importlib
 import sys
 
-from . import (
-    clear_queue, clear_work,
-    error_task, error_worker,
-    sim_task, sim_worker,
-    timer_task, timer_worker,
-    view_queue, view_work,
-)
 import soonq as sq
+
+# Import example modules.
+EXAMPLE_FUNCTIONS = (
+    '.clear_queue', '.clear_work',
+    '.error_task', '.error_worker',
+    '.sim_task', '.sim_worker',
+    '.timer_task', '.timer_worker',
+    '.view_queue', '.view_work',
+)
+EXAMPLE_MODULES = [importlib.import_module(func, package='examples')
+    for func in EXAMPLE_FUNCTIONS]
+EXAMPLE_MODULE_NAMES = [mod.__name__.rsplit('.', maxsplit=1)[-1]
+    for mod in EXAMPLE_MODULES]
+EXAMPLE_MODULE_DESCRIPTIONS = [mod.__doc__.rstrip('\n')
+    for mod in EXAMPLE_MODULES]
+
+
+HELP_OPTIONS = ('-h', '--help')
+OPTIONS = [(HELP_OPTIONS, "Show this message and exit.")]
 
 
 def execute(command):
@@ -15,8 +31,24 @@ def execute(command):
         cmd = getattr(sys.modules[__name__], command)
     except AttributeError:
         sq.echo(f"Example {command!r} not recognized.")
+        return
     cmd()
 
 
+def print_help():
+    sq.echo(f"Usage: python -m examples [OPTIONS] COMMAND [ARGS]...\n")
+    sq.echo("Options:")
+    for opts, desc in OPTIONS:
+        sq.echo(f"  {', '.join(opts)}  {desc}")
+    sq.echo()
+    sq.echo("Commands:")
+    max_modname_len = max(len(mod) for mod in EXAMPLE_MODULE_NAMES)
+    for cmd, descr in zip(EXAMPLE_MODULE_NAMES, EXAMPLE_MODULE_DESCRIPTIONS):
+        sq.echo(f"  {cmd:<{max_modname_len+2}}{descr}")
+
+
 command = sys.argv[1]
-execute(command)
+if command in HELP_OPTIONS:
+    print_help()
+else:
+    execute(command)
